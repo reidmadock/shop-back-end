d2431c53f05a8890090f5e06063cf5e599550fe6
Reid M <reidmadock@gmail.com>
2023-06-02 19:29:10 -0500

    made readme fixed a comment

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,31 +27,7 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err); //post unsuccesful status and return the error.
   }
 });
-/*
-// create new product
-router.post('/', (req, res) => {
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tag_ids) {
-        const productTagIdArr = req.body.tag_ids.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
-*/
+
 router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
